4de52cca62a77901edecb439b1dd2c47d1bd8eef
Sandip <roysandip33290@gmail.com>
2024-04-27 15:23:55 +0530

    indentation fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
  
  export default class App extends Component {
-  name='Sandip';
+  name ='Sandip';
    render() {
      return (
        <div>
