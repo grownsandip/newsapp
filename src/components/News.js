bd5d924362cf7aa09d6bfd6a66263e8efdd6d46d
Sandip <roysandip33290@gmail.com>
2024-04-28 16:59:45 +0530

    added new items and json file for the API

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a new components.
-        <NewsItem/>
+      <div className='container my-3 '>
+        <h2 className='text-center'>Top headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+            <NewsItem title='mytitle'description='decsc'/>
+            </div>
+            <div className="col-md-4">
+            <NewsItem title='mytitle'description='decsc'/>
+            </div>
+            <div className="col-md-4">
+            <NewsItem title='mytitle'description='decsc'/>
+            </div>
+        </div>
       </div>
     )
   }
