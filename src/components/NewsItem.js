b34505727e22bfd708976bca6eb2e38b701e9580
Sandip <roysandip33290@gmail.com>
2024-05-01 19:09:07 +0530

    used states to make the title image and url show  button to go to external websites

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title,description,imageUrl}=this.props;
+        let {title,description,imageUrl,newsUrl}=this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{width:'18rem'}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={imageUrl} className="card-img-top" alt="loading"/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href="/newsDetails" className="btn btn-sn btn-primary">know more</a>
+                            <a href={newsUrl} className="btn btn-sn btn-primary">know more</a>
                         </div>
                 </div>
             </div>
