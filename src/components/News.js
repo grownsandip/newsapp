3290cf3a4aef87a88c07591f778b338750e5a495
Sandip <roysandip33290@gmail.com>
2024-05-05 16:31:05 +0530

    resolved conflicts in current cahnges

--- src/components/News.js
+++ src/components/News.js
@@ -4,8 +4,6 @@ import { Spinner } from "./Spinner";
 import PropTypes from "prop-types";
 <<<<<<< HEAD
 import InfiniteScroll from "react-infinite-scroll-component";
-=======
->>>>>>> newsapp/master
 
 export class News extends Component {
   static defaultProps = {
@@ -49,39 +47,6 @@ export class News extends Component {
   }
   async componentDidMount() {
   this.updateNews();
-=======
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-    };
-  }
-  async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8de7740bb30498e93e9cfda716fd8a0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-  }
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8de7740bb30498e93e9cfda716fd8a0&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
->>>>>>> newsapp/master
   }
   handleNext = async () => {
     this.setState({ page: this.state.page + 1 });
@@ -136,58 +101,4 @@ export class News extends Component {
   }
 }
 export default News
-=======
-  render() {
-    return (
-      <div className="container my-3 ">
-        <h2 className="text-center">Top headlines</h2>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 30) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 80)
-                        : ""
-                    }
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    date={element.publishedAt}
-                    author={element.author}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePrev}
-          >
-            &larr;Previous{" "}
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
-            }
-            type="button"
-            class="btn btn-dark"
-            onClick={this.handleNext}
-          >
-            Next &rarr;
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
 
-export default News;
->>>>>>> newsapp/master
