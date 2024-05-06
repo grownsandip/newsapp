b5820ac6daf93bdd54d0da0810483a391fed9d67
Sandip <roysandip33290@gmail.com>
2024-05-06 16:46:26 +0530

    hidden api key with env variables

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
  
  export default class App extends Component {
   pageSize=5;
+  apiKey=process.env.REACT_APP_NEWS_API;
   state={
     progress:0
   }
@@ -30,13 +31,13 @@ import {
         progress={this.state.progress}
       />
         <Routes>
-        <Route exact path="/" element={<News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} counrty="us" category="general"/>}/>
-        <Route exact path="/business" element={<News setProgress={this.setProgress}  key="business"pageSize={this.pageSize} counrty="in" category="business"/>}/>
-        <Route exact path="/science" element={<News setProgress={this.setProgress}  key="science"pageSize={this.pageSize} counrty="in" category="science"/>}/>
-        <Route exact path="/sports/" element={<News setProgress={this.setProgress}  key="sports"pageSize={this.pageSize} counrty="in" category="sports"/>}/>
-        <Route exact path="/health" element={<News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} counrty="in" category="health"/>}/>
-        <Route exact path="/technology"element={<News setProgress={this.setProgress}  key="technology"pageSize={this.pageSize} counrty="in" category="technology"/>}/>
-        <Route exact path="/entertainment" element={<News setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} counrty="in" category="entertainment"/>}/>
+        <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} counrty="us" category="general"/>}/>
+        <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="business"pageSize={this.pageSize} counrty="in" category="business"/>}/>
+        <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science"pageSize={this.pageSize} counrty="in" category="science"/>}/>
+        <Route exact path="/sports/" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports"pageSize={this.pageSize} counrty="in" category="sports"/>}/>
+        <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize} counrty="in" category="health"/>}/>
+        <Route exact path="/technology"element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology"pageSize={this.pageSize} counrty="in" category="technology"/>}/>
+        <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={this.pageSize} counrty="in" category="entertainment"/>}/>
         </Routes>
         </Router>
        </div>
--- src/components/News.js
+++ src/components/News.js
@@ -57,7 +57,7 @@ export class News extends Component {
   };
   fetchMoreData= async ()=>{
      this.setState({page:this.state.page+1});
-     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8de7740bb30498e93e9cfda716fd8a0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
      let data = await fetch(url);
      let parsedData = await data.json();
      this.setState({
